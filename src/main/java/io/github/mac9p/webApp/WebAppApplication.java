292be56307853276459a152781490273d2dda6f5
Zarkii <mleko493@wp.pl>
2021-05-06 00:35:20 +0200

    Initial commit

---
+++ src/main/java/io/github/mac9p/webApp/WebAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.mac9p.webApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppApplication.class, args);
+	}
+
+}
